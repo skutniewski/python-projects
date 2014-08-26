ab8db833fbec66f68ff56781e0b8da4e8c15f465
Stella ros <skutniewski07@gmail.com>
2014-08-26 16:54:23 -0400

    i wrote code print and function

---
+++ stellas program.py
@@ -0,0 +1,11 @@
+print('function')
+print('hello')
+print('DO you like minecraft?')
+answer = input()
+print('You answered:,answer')
+
+if answer.startswith('yes'):
+   print('AAAwwweeesssooommmeee!!!!!!!!!!!!!!!!!!!!!!)
+
+elif
+    
