a2278e255c4016cf6730cca8cc50bdb5ff30d4a9
Stella ros <skutniewski07@gmail.com>
2014-09-05 20:06:29 -0400

    aosome code

---
+++ puppy quiz.py
@@ -0,0 +1,3 @@
+print('welcome to the puppy quiz')
+print('hello what is your name')
+print('good luck')
---
+++ stellas asomesnes program.py
@@ -0,0 +1,5 @@
+print('stella you are so nice')
+
+print('you are so nice computer')
+
+input('you are so nice')
