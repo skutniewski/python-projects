76a28c133973f7da91745dcf4cc256dcbaae75ed
Stella ros <skutniewski07@gmail.com>
2014-09-30 16:35:29 -0400

    just did javastript

---
+++ artist 1.py
@@ -0,0 +1,21 @@
+'''
+
+moveForward(100)
+turnForward(90)
+moveForward(100)
+
+
+'''
+
+
+
+from turtle import Turtle
+slon = Turtle()
+
+slon.pensize(7)
+slon.pencolor('hot pink')
+
+sloan.right(90)
+sloan.forward(100)
+sloan.right(90)
+
